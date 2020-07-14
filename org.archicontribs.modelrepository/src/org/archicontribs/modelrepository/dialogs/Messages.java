e73d74bd7244df131336cf305b7de02486433c3b
Phillipus <p.beauvoir@dadabeatnik.com>
2020-07-14 14:09:40 +0100

    Allow dot and forward slash characters in branch names
    * Allow user to type these
    * Show error message for invalid strings

--- org.archicontribs.modelrepository/src/org/archicontribs/modelrepository/dialogs/Messages.java
+++ org.archicontribs.modelrepository/src/org/archicontribs/modelrepository/dialogs/Messages.java
@@ -14,7 +14,13 @@ public class Messages extends NLS {
 
     public static String AddBranchDialog_3;
 
+    public static String AddBranchDialog_4;
+
+    public static String AddBranchDialog_5;
+
     public static String AddBranchDialog_6;
+
+    public static String AddBranchDialog_7;
 
     public static String CloneInputDialog_0;
 
