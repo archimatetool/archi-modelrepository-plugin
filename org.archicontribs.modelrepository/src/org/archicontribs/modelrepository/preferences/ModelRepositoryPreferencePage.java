b6706caa70fa02795058fa6739a28c6c5174e7dc
Phillipus <p.beauvoir@dadabeatnik.com>
2020-11-19 10:54:29 +0000

    [Preferences] Remove excess margins and fix tables not updating on Mac
    * Each Page should have 0 pixels left and right margins as per the JavaDoc for PreferencePage#createContents()

--- org.archicontribs.modelrepository/src/org/archicontribs/modelrepository/preferences/ModelRepositoryPreferencePage.java
+++ org.archicontribs.modelrepository/src/org/archicontribs/modelrepository/preferences/ModelRepositoryPreferencePage.java
@@ -86,7 +86,9 @@ implements IWorkbenchPreferencePage, IPreferenceConstants {
         PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, HELP_ID);
 
         Composite client = new Composite(parent, SWT.NULL);
-        client.setLayout(new GridLayout());
+        GridLayout layout = new GridLayout();
+        layout.marginWidth = layout.marginHeight = 0;
+        client.setLayout(layout);
         
         // User details
         Group userDetailsGroup = new Group(client, SWT.NULL);
